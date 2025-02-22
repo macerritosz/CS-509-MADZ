77c79b0b58f1f9abcdf87158ff2b00693a7c0c25
macerritosz <macerritos@wpi.edu>
2025-02-22 14:18:14 -0500

    Spring Backend & Vite Frontend Setup

---
+++ backend/src/main/java/com/wpi/cs509madz/Cs509MadzApplication.java
@@ -0,0 +1,13 @@
+package com.wpi.cs509madz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs509MadzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs509MadzApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/wpi/cs509madz/Cs509MadzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpi.cs509madz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs509MadzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
