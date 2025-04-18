6f4f0f58037a9165f25562176fdd8d86f7bc6722
macerritosz <macerritos@wpi.edu>
2025-04-18 14:25:28 -0400

    Added Loading Objects and Setup Data Display Logic

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+
+export default function Loading() {
+    return (
+        <div >
+            <div className={"svg-bounce w-[75px] h-[75px]"}>
+                <img src="/airplane-in-flight-thin-svgrepo-com.svg" alt="earth spin"/>
+            </div>
+            <div className={"svg-container w-[150px] h-[150px]"}>
+                <img src="/planet-earth-svgrepo-com.svg" alt="earth spin"/>
+            </div>
+        </div>
+    )
+}
