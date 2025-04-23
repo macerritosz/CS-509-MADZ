79d1ef77f0d54818deb650a65d067287de75edf1
Darlina Phan <darlinaphan@gmail.com>
2025-04-23 15:06:40 -0400

    Fix SouthwestsRepository

--- backend/src/main/java/com/wpi/cs509madz/repository/SouthwestsRepository.java
+++ backend/src/main/java/com/wpi/cs509madz/repository/SouthwestsRepository.java
@@ -4,25 +4,43 @@ import com.wpi.cs509madz.model.Flight;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class SouthwestsRepository {
 
-    private static JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-    public JdbcTemplate getJdbcTemplate(){
-        return jdbcTemplate;
-    }
     @Autowired
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
-        SouthwestsRepository.jdbcTemplate = jdbcTemplate;
+    public SouthwestsRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public static List<Flight> findAll() {
+
+    public List<Flight> findAll() {
         String sql = "select * from southwests";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Flight.class));
+
+        RowMapper<Flight> rowMapper = new RowMapper<Flight>() {
+            @Override
+            public Flight mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Flight flight = new Flight();
+                flight.setId(rs.getInt("id"));
+                flight.setArriveAirport(rs.getString("ArriveAirport"));
+                flight.setDepartAirport(rs.getString("DepartAirport"));
+                flight.setArriveDateTime(rs.getString("ArriveDateTime"));
+                flight.setDepartDateTime(rs.getString("DepartDateTime"));
+                flight.setFlightNumber(rs.getString("FlightNumber"));
+
+                return flight;
+            }
+        };
+
+        return jdbcTemplate.query(sql, rowMapper);
     }
 }
