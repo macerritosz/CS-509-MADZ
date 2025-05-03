fc67c9cb3fee587199e27fa00701649ea49f3cfb
macerritosz <macerritos@wpi.edu>
2025-05-03 11:09:31 -0400

    Added Booking Repository and Modified other Repositorie for booking use

--- backend/src/main/java/com/wpi/cs509madz/repository/SouthwestsRepository.java
+++ backend/src/main/java/com/wpi/cs509madz/repository/SouthwestsRepository.java
@@ -4,8 +4,13 @@ import com.wpi.cs509madz.model.Flight;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -23,6 +28,52 @@ public class SouthwestsRepository {
 
     public static List<Flight> findAll() {
         String sql = "select * from southwests";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Flight.class));
+
+        RowMapper<Flight> rowMapper = new RowMapper<Flight>() {
+            @Override
+            public Flight mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Flight flight = new Flight();
+                flight.setId(rs.getInt("id"));
+                flight.setArriveAirport(rs.getString("ArriveAirport"));
+                flight.setDepartAirport(rs.getString("DepartAirport"));
+                flight.setArriveDateTime(rs.getString("ArriveDateTime"));
+                flight.setDepartDateTime(rs.getString("DepartDateTime"));
+                flight.setFlightNumber(rs.getString("FlightNumber"));
+
+                return flight;
+            }
+        };
+
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public List<Flight> getFlightByID(int id) {
+        String sql = "select * from deltas where id = ?";
+
+        RowMapper<Flight> rowMapper = new RowMapper<Flight>() {
+            @Override
+            public Flight mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Flight flight = new Flight();
+                flight.setId(rs.getInt("id"));
+                flight.setArriveAirport(rs.getString("ArriveAirport"));
+                flight.setDepartAirport(rs.getString("DepartAirport"));
+                flight.setArriveDateTime(rs.getString("ArriveDateTime"));
+                flight.setDepartDateTime(rs.getString("DepartDateTime"));
+                flight.setFlightNumber(rs.getString("FlightNumber"));
+
+                return flight;
+            }
+        };
+
+        return jdbcTemplate.query(sql, new PreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps) throws SQLException {
+                        ps.setString(1, String.valueOf(id));
+                    }
+                },
+                rowMapper
+        );
     }
 }
