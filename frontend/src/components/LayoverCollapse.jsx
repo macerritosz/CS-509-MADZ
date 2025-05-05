287eb1f1604b95789e2117bac997a957d4d3a543
macerritosz <macerritos@wpi.edu>
2025-05-05 14:17:45 -0400

    Added search for selection, and flight page formatting

--- frontend/src/components/LayoverCollapse.jsx
+++ frontend/src/components/LayoverCollapse.jsx
@@ -3,13 +3,15 @@ import LayoverCard from "./LayoverCard.jsx";
 
 export default function LayoverCollapse(props) {
 
+
+
     return (
         <div className="p-2">
             <div className="flex flex-wrap gap-4">
                 {
                     props.layovers.map((layovers, index) => (
                         <LayoverCard
-                            key={index}
+                            key={`LayoverCard-${layovers.departureDateTime}-${layovers.id}`}
                             layoverInformation={layovers}
                         />
                     ))
